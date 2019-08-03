2af6e86dc3348717362811d6acb679628fc2418e
Artur Jakubowski <jakubowski1005@gmail.com>
2019-08-03 15:27:21 +0200

    Add some test component

---
+++ src/SomeComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SomeComponent extends Component {
+
+    render() {
+        return (
+            <div className="someComponent">
+                Some component content
+            </div>
+        );
+    }
+}
+
+export default SomeComponent;
