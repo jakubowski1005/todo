7773496c1647a962b79ae60c8b9f2731a530744a
Artur Jakubowski <jakubowski1005@gmail.com>
2019-08-04 04:07:32 +0200

    Fix bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-//import Counter from './components/counter/Counter';
 import TodoApp from './components/todo/TodoApp.jsx';
 import './App.css';
 import './bootstrap.css'
@@ -9,7 +8,6 @@ class App extends Component {
     render() {
         return (
             <div>
-                {/* <Counter /> */}
                 <TodoApp />
             </div>
         );
